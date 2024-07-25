24fee963fe4fca37a3417e552b6e3c3236a833d4
Erredebe <acastro.mecemsa@gmail.com>
2024-07-25 15:28:10 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { DocGeneratorService } from './doc-generator.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-doc';
+  constructor(private docGeneratorService: DocGeneratorService) {}
+
+  downloadTableDocument() {
+    this.docGeneratorService.generateTableDocument();
+  }
+
+  downloadDocument(): void {
+    this.docGeneratorService.generateWordDocument();
+  }
+  downloadLandscape(): void {
+    this.docGeneratorService.createLandscapeDocument();
+  }
 }
