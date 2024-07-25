c6386ca6d22d651d399c7cce8caa4a4bf4260347
Erredebe <acastro.mecemsa@gmail.com>
2024-07-25 15:39:51 +0200

    bad cut and paste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { DocGeneratorService } from './doc-generator.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   constructor(private docGeneratorService: DocGeneratorService) {}
