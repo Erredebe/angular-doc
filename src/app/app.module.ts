e37fabdba497ab0f2c5ed19d5f131d6e5d91b970
Erredebe <acastro.mecemsa@gmail.com>
2024-08-06 14:00:21 +0200

    removed unnecesay component and added scss style on docx-uploader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { JsonReaderComponent } from './json-reader/json-reader.component';
+
 import { JsonToolModule } from './json-tool/json-tool.module';
 import { CoreModule } from './core/core.module';
 import { StorageService } from './core/storage/storage.service';
 import { DocxToolModule } from './docx-tool/docx-tool.module';
 
 @NgModule({
-  declarations: [AppComponent, JsonReaderComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
